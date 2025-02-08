adb64041594f4b25127ac89430bcea4c1c217397
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2025-02-08 13:14:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { authenticateUser } = require('./auth');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+// Middleware para interpretar JSON
+app.use(bodyParser.json());
+
+// Middleware para servir arquivos estáticos (HTML, CSS, etc.)
+app.use(express.static(__dirname));
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    authenticateUser(username, password, (err, user) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro no servidor' });
+        }
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(401).json({ error: 'Credenciais inválidas.' });
+        }
+    });
+});
+
 
 // Rota para a raiz que serve o index.html
 app.get('/', (req, res) => {
