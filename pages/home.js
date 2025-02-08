7a6948d44c34fd8c6b32e086d6f2e5669fbbc176
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2025-02-08 14:46:57 -0300

    Delete pages directory

--- pages/home.js
+++
@@ -1,9 +0,0 @@
-const storedLogin = JSON.parse(sessionStorage.getItem('login'));
-
-window.addEventListener('load', function() {
-    if (storedLogin && storedLogin.login) {
-        window.alert('Seja bem-vindo, ' + storedLogin.user.username + '!');
-    } else {
-        window.location.href = '../index.html';
-    }
-}); 
